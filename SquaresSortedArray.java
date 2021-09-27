18c68fd740dcaf5876b58e9b205cbbc519454fb7
Aritra995 <aritrasiliguri@gmail.com>
2021-09-27 15:51:31 +0530

    Solution Leetcode 977

---
+++ SquaresSortedArray.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+import javax.swing.SortOrder;
+
+public class SquaresSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        for(int i =0;i < nums.length;i++){
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    // prototype main
+    public static void main(String[] args) {
+        int[] nums = {-7,-3,2,3,11};
+        SquaresSortedArray sortedArray = new SquaresSortedArray();
+        nums = sortedArray.sortedSquares(nums);
+        for (int i : nums) {
+            System.out.println(i);
+        }
+    }
+}
